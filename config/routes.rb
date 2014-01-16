ee846167a425c4b114f9c9e49bc7f342cbde3ccd
andy chen <chen.a.andy@gmail.com>
2014-01-15 21:32:37 -0800

    Added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DEMO::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -9,10 +11,9 @@ DEMO::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+	root :to => 'welcome#index'
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +49,7 @@ DEMO::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+    root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
